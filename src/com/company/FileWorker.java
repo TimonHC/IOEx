f4db14702d307ca34bcc5a416db313cb12bd2d5c
TimonHC <timofeyyevtushenko@gmail.com>
2019-11-27 18:10:49 +0200

    27.11.19

--- src/com/company/FileWorker.java
+++ src/com/company/FileWorker.java
@@ -3,29 +3,29 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class FileWorker {
 
     public static void write(String fileName, String text) {
-        //Определяем файл
+        //define the file
         File file = new File(fileName);
 
         try {
-            //проверяем, что если файл не существует то создаем его
+            //checks is the file exists and creates it if not
             if(!file.exists()){
                 file.createNewFile();
             }
 
-            //PrintWriter обеспечит возможности записи в файл
+            //PrintWriter provides the possibility to write in the file
             PrintWriter out = new PrintWriter(file.getAbsoluteFile());
 
             try {
-                //Записываем текст у файл
+                //writes text in the file
                 out.print(text);
             } finally {
-                //После чего мы должны закрыть файл
-                //Иначе файл не запишется
+                //we need to close the file, bcause data will be not written
                 out.close();
             }
         } catch(IOException e) {
@@ -33,13 +33,25 @@ public class FileWorker {
         }
     }
     public static String textEveryWordFirstLetterToUpperCase(String text) {
+        //defines the delimiter between to concat the edited words
         StringJoiner joiner = new StringJoiner(" ");
+        //loop through text and splits it on words
         for (String word : text.split(" ")) {
+            //changes first letter in every word  to uppercase
             word = word.substring(0, 1).toUpperCase() + word.substring(1);
+            //concats all edited words to text
             joiner.add(word);
             text = joiner.toString();
         }
         return text;
     }
-
+    public static String deleteFromTextSelectedArg(String text){
+        Scanner scanner = new Scanner(System.in);
+        //select string to delete from source text
+        String toDelete = scanner.nextLine();
+        //loop through source text and replace choosed string to delete and space after it with empty string
+        text = text.replace(toDelete+" ", "");
+        System.out.println(text);
+        return text;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,11 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String buffer = "";
         Scanner scanner = new Scanner(new File("Files\\Shakespeare.txt"));
-
         while(scanner.hasNext()){
         buffer += (scanner.nextLine() + "\r\n");}
+
         FileWorker.write("Files\\ShakspeareEdited.txt", FileWorker.textEveryWordFirstLetterToUpperCase(buffer));
-        
+        FileWorker.write("Files\\TextWithDeletedWords.txt", FileWorker.deleteFromTextSelectedArg(buffer));
+
     }
 }
